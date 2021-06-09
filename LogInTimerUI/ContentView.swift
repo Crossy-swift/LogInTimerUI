db87cabc14cb087893a3691ea58c3e4a21ddd2ee
Crossy <Crossworm2011@gmail.com>
2021-06-09 09:47:21 +0300

    Project start

---
+++ LogInTimerUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LogInTimerUI
+//
+//  Created by Артем Репин on 09.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LogInTimerUI/LogInTimerUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogInTimerUIApp.swift
+//  LogInTimerUI
+//
+//  Created by Артем Репин on 09.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct LogInTimerUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
